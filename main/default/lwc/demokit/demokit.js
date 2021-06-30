97e0a16792192cfa9d81fb1b7deb4739de81fc20
Will Thompson <williamjamesthompson@gmail.com>
2021-06-30 10:44:13 -0500

    Pushing to github

---
+++ main/default/lwc/demokit/demokit.js
@@ -0,0 +1,61 @@
+import { LightningElement, wire, track, api } from 'lwc'
+import { loadScript } from 'lightning/platformResourceLoader'
+import threekit from '@salesforce/resourceUrl/playerbundle'
+// this is the right bundle - playerbundle
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi'
+import ASSET_ID_FIELD from '@salesforce/schema/Product2.TKB2B__Threekit_Id__c'
+import AUTH_TOKEN_FIELD from '@salesforce/schema/Product2.auth_token__c'
+
+const fields = [ASSET_ID_FIELD, AUTH_TOKEN_FIELD]
+
+export default class Demokit extends LightningElement {
+  @api recordId
+  @wire(getRecord, { recordId: '$recordId', fields })
+  // @wire()
+  product
+
+  get assetId () {
+    console.log(this.product)
+    return getFieldValue(this.product.data, ASSET_ID_FIELD)
+  }
+
+  get authToken () {
+    // replace getFieldValue with getRecord and see if we can get a field in that record?
+    return getFieldValue(this.product.data, AUTH_TOKEN_FIELD)
+  }
+  renderedCallback () {
+    // run once check
+
+    // call script loaders...
+    Promise.all([loadScript(this, threekit + '/player-bundle.js')])
+      .then(() => {
+        console.log('loaded')
+        window
+          .threekitPlayer({
+            authToken: this.authToken,
+            el: this.template.querySelector('.tkplayer'),
+            //   stageId: '27b9cd7e-2bb2-4a18-b788-160743eb4b33',
+            //   assetId: "1d5226e9-af8c-472d-af6a-bc4867a4bf5c",
+            assetId: this.assetId,
+            showConfigurator: true,
+            showAR: true
+            //   cache: {
+            //     maxAge: 500,
+            //     scope: "v1.0"
+            //   }
+          })
+          .then(async player => {
+            window.player = player
+            let configurator = await player.getConfigurator()
+
+            await player.when('loaded')
+
+            let attrs = configurator.getAttributes()
+            console.log(attrs)
+          })
+      })
+      .catch(error => {
+        console.log('Error', error)
+      })
+  }
+}
